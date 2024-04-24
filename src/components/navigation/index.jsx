fbf3b2b31ba3aeca2a9e7fa85b9e94a6707b1442
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-23 22:35:18 -0400

    Created Home Page and added the 3d Wizard with the animation. Next step working on implementing the navigation bar.

---
+++ src/components/navigation/index.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily:{
+        inter: ['var(--font-inter)']
       },
+      colors:{
+        background: 'rgb(var(--background))',
+        foreground: 'rgb(var(--foreground))',
+        muted: 'rgb(var(--muted))',
+        accent: 'rgb(var(--accent))',
+
+      }
     },
   },
   plugins: [],
