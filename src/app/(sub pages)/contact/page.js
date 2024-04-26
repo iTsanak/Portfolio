fa509de71ea415f5c1ff78bad7ab7301dd20372a
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-26 00:36:26 -0400

    Completed Contact Form page

---
+++ src/app/(sub pages)/contact/page.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import bg from "../../../../public/background/contact-background.png";
+import RenderModel from "@/components/RenderModel";
+import HatModel from "@/components/models/HatModel";
+import AboutDetails from "@/components/about";
+import Form from "@/components/contact/Form";
+
+
+export default function Contact() {
+  return (
+    <>
+      <Image src={bg} alt="background-image"  className="-z-50 fixed top-0 left-0 w-full
+       h-full object-cover object-center opacity-25" />
+
+      <article className="relative w-full flex flex-col items-center justify-center space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+          <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+            Summon the Mage
+          </h1>
+          <p className="text-center font-light">
+          Greetings, noble traveler of the digital realm! Step into the enchanted circle and let your words
+          weave spells across the cosmic tapestry. Whether you seek to forge alliances, unlock ancient 
+          mysteries, or simply share your adventures, your messages are precious scrolls in this mystical domain.
+
+          Use the form below to send your missives through the ethereal network, and await the whisper of magic 
+          in response. 
+          </p>
+        </div>
+        <Form />
+      </article>
+        
+      
+    </>
+  );
+}
