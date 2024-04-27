97b355f57ef462d273c1a003164ba30f7dc51f50
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-27 12:07:21 -0400

    Contact Page Completed

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -12,12 +12,12 @@ export default function Contact() {
       <Image src={bg} alt="background-image"  className="-z-50 fixed top-0 left-0 w-full
        h-full object-cover object-center opacity-25" />
 
-      <article className="relative w-full flex flex-col items-center justify-center space-y-8">
-        <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full  sm:*:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
             Summon the Mage
           </h1>
-          <p className="text-center font-light">
+          <p className="text-center font-light text-sm xs:text-base max-w-xl">
           Greetings, noble traveler of the digital realm! Step into the enchanted circle and let your words
           weave spells across the cosmic tapestry. Whether you seek to forge alliances, unlock ancient 
           mysteries, or simply share your adventures, your messages are precious scrolls in this mystical domain.
