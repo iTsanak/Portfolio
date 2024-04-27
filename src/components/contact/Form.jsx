19df613aa78063e3ba1337fd2a9391ee7ef2ee38
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-26 22:10:23 -0400

    Contact form completed and added the validation UI messages

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -2,10 +2,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
+import { Toaster, toast } from 'sonner'
+
+
+
+
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-/////////////////2hr 36min///////////////////////////////
   const sendEmail = (params) => {
+
+    const toastId = toast.loading('Sending your message...')
     emailjs
       .send(
         process.env.NEXT_PUBLIC_SERVICE_ID, 
@@ -19,10 +25,10 @@ export default function Form() {
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          toast.success('Message cast successfully', {id: toastId});
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          toast.success('There was an error while casting your message, please try again!', {id: toastId});
         },
       );
   };
@@ -36,26 +42,55 @@ export default function Form() {
     }
     sendEmail(templateParams);
   };
-  console.log(errors);
+  
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='max-w-md w-full flex flex-col items-center space-y-4'>
-      <input type="text" placeholder="Name" {...register("Name", {required: true})} className='w-full p-2 
-      rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
+    <>
+      <Toaster richColors={true}/>
+      <form onSubmit={handleSubmit(onSubmit)} className='max-w-md w-full flex flex-col items-center space-y-4'>
 
-      <input type="email" placeholder="Email" {...register("Email", {required: true})} className='w-full p-2 
-      rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
-      
-      <textarea {...register("Message", {max: 256, min: 5})} placeholder="Message" className='w-full p-2 
-      rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' />
+        <input type="text" placeholder="Name" {...register("Name", {required: 'This field is required!',
+        minLength:{
+          value: 3,
+          message: 'Name should be at least 3 characters'
 
-      <input
-      value="Cast your Message"
-      className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
-      hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50
-      cursor-pointer capitalize' 
-      type="submit"
-      />
-    </form>
+        }})} className='w-full p-2 
+        rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+        />
+        {
+          errors.Name && <span className='inline-block self-start text-accent'>{errors.Name.message}</span>
+        }
+        
+        <input type="email" placeholder="Email" {...register("email", {required: 'This field is required!'})} 
+        className='w-full p-2   
+        rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+        />
+        {
+          errors.email && <span className='inline-block self-start text-accent'>{errors.email.message}</span>
+        }
+
+        <textarea {...register("Message", {required: 'This field is required!', maxLength: {
+            value: 500,
+            message: "Message should be less than 500 characters"
+          }, minLength: {
+            value: 30,
+            message: "Message should be more than 30 characters"
+          } })} 
+          placeholder="Message" className='w-full p-2 
+          rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg' 
+        />
+        {
+          errors.Message && <span className='inline-block self-start text-accent'>{errors.Message.message}</span>
+        }
+
+        <input
+        value="Cast your Message"
+        className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
+        hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50
+        cursor-pointer capitalize' 
+        type="submit"
+        />
+      </form>
+    </>
   );
 }
