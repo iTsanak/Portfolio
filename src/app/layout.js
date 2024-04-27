52a1e53f7562ac7236f4719aa499c157a7c342ef
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-26 22:11:26 -0400

    Working on the navigation buttons to resize properly for mobile responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
 
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>
         {children}
         <FireFliesBackground />
+        
       </body>
     </html>
   );
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,11 +2,20 @@
 import { BtnList } from '@/app/data'
 import React from 'react'
 import NavButton from './NavButton';
+import useScreenSize from '../hooks/useScreenSize';
 
-// Divide the circle in (8) equal parts 
-const angleIncrement = 360 / BtnList.length;
 
 const Navigation = () => {
+
+    // Divide the circle in (8) equal parts 
+    const angleIncrement = 360 / BtnList.length;
+    const screenSize = useScreenSize(); 
+    
+    // Check the screen size for mobile and desktop for responsiveness
+    const isLarge = screenSize >= 1024;
+    const isMedium = screenSize >= 768 && screenSize < 1024;
+
+
     return (
     <div className='w-full fixed h-screen flex items-center justify-center z-50'>
         <div className='w-max flex items-center justify-center relative hover:pause animate-spin-slow group'>
@@ -14,7 +23,9 @@ const Navigation = () => {
             BtnList.map((btn, index) => {
                 // Convert the angle to radians for different button button by multiplying with the index
                 const angleRad = (angleIncrement * index * Math.PI) / 180;
-                const radius = 'calc(20vw - 1rem)';
+                const radius = isLarge ? 'calc(20vw - 1rem)' : isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem)'
+
+
                 const x = `calc(${radius} * ${Math.cos(angleRad)})`;
                 const y = `calc(${radius} * ${Math.sin(angleRad)})`;
 
