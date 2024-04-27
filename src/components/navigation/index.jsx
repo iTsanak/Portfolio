e47885b4feb4cbd38cac2c70895b0f12ebdf55c3
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-27 10:11:04 -0400

    Adapting the Navigation buttons for each screen size.

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -3,6 +3,7 @@ import { BtnList } from '@/app/data'
 import React from 'react'
 import NavButton from './NavButton';
 import useScreenSize from '../hooks/useScreenSize';
+import ResponsiveComponent from '../ResponsiveComponent';
 
 
 const Navigation = () => {
@@ -17,23 +18,54 @@ const Navigation = () => {
 
 
     return (
-    <div className='w-full fixed h-screen flex items-center justify-center z-50'>
-        <div className='w-max flex items-center justify-center relative hover:pause animate-spin-slow group'>
-        {
-            BtnList.map((btn, index) => {
-                // Convert the angle to radians for different button button by multiplying with the index
-                const angleRad = (angleIncrement * index * Math.PI) / 180;
-                const radius = isLarge ? 'calc(20vw - 1rem)' : isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem)'
+        <div className='w-full fixed h-screen flex items-center justify-center z-50'>
+            <ResponsiveComponent>
+            {
+                ({screenSize}) => {
+                    return screenSize && screenSize >= 480 ? (
+                        <div className='w-max flex items-center justify-center relative hover:pause 
+                        animate-spin-slow group'>
+                            {
+                                BtnList.map((btn, index) => {
+                                    // Convert the angle to radians for different button button by multiplying with the index
+                                    const angleRad = (angleIncrement * index * Math.PI) / 180;
+                                    const radius = isLarge 
+                                    ? 'calc(20vw - 1rem)' 
+                                    : isMedium 
+                                    ? 'calc(30vw - 1rem)' 
+                                    : 'calc(40vw - 1rem)';
+                                    const x = `calc(${radius} * ${Math.cos(angleRad)})`;
+                                    const y = `calc(${radius} * ${Math.sin(angleRad)})`;
 
+                                    return <NavButton key={btn.label} x={x} y={y} {...btn} />
+                                })
+                            }
+                        </div>
+                    ) : (
+                    <>
+                        <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-4
+                         item-start xs:items-center justify-center relative group'>
+                            {
+                                BtnList.slice(0, BtnList.length/2).map((btn, index) => {
+                                    return <NavButton key={btn.label} x={0} y={0} {...btn} />
+                                })
+                            }
+                        </div>
 
-                const x = `calc(${radius} * ${Math.cos(angleRad)})`;
-                const y = `calc(${radius} * ${Math.sin(angleRad)})`;
-
-                return <NavButton key={btn.label} x={x} y={y} {...btn} />
-            })
-        }
-        </div>   
-    </div>
+                        <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-4 items-end
+                         xs:items-center justify-center relative group'>
+                            {
+                                BtnList.slice(BtnList.length/2, BtnList.length).map((btn, index) => {
+                                    return <NavButton key={btn.label} x={0} y={0} {...btn} labelDirection='left' />
+                                })
+                            }
+                        </div>
+                    </>
+                    )
+                }
+            }
+            </ResponsiveComponent>
+        </div>
     )
 }
 
