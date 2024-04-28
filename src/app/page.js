44802bd09c7998785c9b49d2447bf345169c7970
iTsanak <itsanachtsidis@myseneca.ca>
2024-04-27 22:30:50 -0400

    Added the background sound component, implemented the play music consent cookie and added the toggle option button for the music. Shows in all pages

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between relative ">
       <Image src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-20  " />
     
-      <div className="w-full h-screen">
+      <div className="w-full h-screen ">
         {/* nav and 3d model*/}
         <Navigation />
         
